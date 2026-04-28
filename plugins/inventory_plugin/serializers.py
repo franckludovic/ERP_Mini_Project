8e911d2ba630c79a9d554ec3ae000fdd2a78b29b
PahoJosue <pahotchaptchet4@gmail.com>
2026-04-28 13:36:03 +0100

    Implemented part of materials and serializers

--- plugins/inventory_plugin/serializers.py
+++ plugins/inventory_plugin/serializers.py
@@ -1,16 +1,6 @@
 from rest_framework import serializers
 from .models import *
 
-class SupplierSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Supplier
-        fields = '__all__'
-    
-class MaterialSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Material
-        fields = '__all__'
-
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
@@ -27,4 +17,61 @@ class UpdateProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Product
+        fields = '__all__'
+    
+class addOrRemoveProductSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+# -------- MATERIAL --------
+class MaterialSerializer(serializers.ModelSerializer):
+    supplier = serializers.CharField(source="supplier.name", read_only=True)
+
+    class Meta:
+        model = Material
+        fields = '__all__'
+
+class GetSingleMaterialSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+class DeleteMaterialSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+class UpdateMaterialSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+
+    class Meta:
+        model = Material
+        fields = '__all__'
+    
+class addOrRemoveMaterialSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+    quantity = serializers.IntegerField()
+
+    class Meta:
+        model = Material
+        fields = '__all__'
+
+# -------- SUPPLIER --------
+class SupplierSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Supplier
+        fields = '__all__'
+
+class GetSingleSupplierSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+class DeleteSupplierSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+
+class UpdateSupplierSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField()
+
+    class Meta:
+        model = Supplier
         fields = '__all__'
--- plugins/inventory_plugin/views.py
+++ plugins/inventory_plugin/views.py
@@ -54,6 +54,38 @@ def updateProduct(request):
             return Response({"Error": "Product with this id not found"}, status=status.HTTP_404_NOT_FOUND)
     return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
 
+#Add product quantity
+@api_view(['PATCH'])
+def addProductQuantity(request):
+    serializer = addOrRemoveProductSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        product_id = serializer.validated_data['id']
+        try:
+            product = Product.objects.get(id=product_id)
+            currentQuantity = product.quantity_in_stock
+            newQuantity = currentQuantity + serializer.validated_data['quantity']
+            serializer.update(product, {"quantity_in_stock" : newQuantity})
+            return Response({"Message": f"Quantity Successfully updated for product with id {product_id}"})
+        except Product.DoesNotExist:
+            return Response({"Error": "Product with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Reduce product quantity
+@api_view(['PATCH'])
+def reduceProductQuantity(request):
+    serializer = addOrRemoveProductSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        product_id = serializer.validated_data['id']
+        try:
+            product = Product.objects.get(id=product_id)
+            currentQuantity = product.quantity_in_stock
+            newQuantity = currentQuantity - serializer.validated_data['quantity']
+            serializer.update(product, {"quantity_in_stock" : newQuantity})
+            return Response({"Message": f"Quantity Successfully updated for product with id {product_id}"})
+        except Product.DoesNotExist:
+            return Response({"Error": "Product with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
 #Delete product
 @api_view(['DELETE'])
 def deleteProduct(request):
@@ -69,4 +101,154 @@ def deleteProduct(request):
     return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
 
 ###############-----------MATERIAL--------------############
+#Create a new material
+@api_view(['POST'])
+def createMaterial(request):
+    serializer = MaterialSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"Message": "Material successfully created"})
+
+#Retrieve all the materials
+@api_view(['GET'])
+def getAllMaterials(request):
+    materials = Material.objects.select_related('supplier').all()
+    serializer = MaterialSerializer(materials, many=True) #many=True means that we are serializing multiple items, else put it to false
+    return Response(serializer.data)
+
+#Retrieve a single material
+@api_view(['GET'])
+def getSingleMaterial(request):
+    serializer = GetSingleMaterialSerializer(data=request.data)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            serializer_data = MaterialSerializer(material, many=False)
+            return Response(serializer_data.data)
+        except Product.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Update Material
+@api_view(['PATCH'])
+def updateMaterial(request):
+    serializer = UpdateMaterialSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            updatedMaterial = serializer.update(material, serializer.validated_data)
+            return Response({"Message": f"Material with id {material_id} successfully updated"})
+        except Material.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Add product quantity
+@api_view(['PATCH'])
+def addMaterialQuantity(request):
+    serializer = addOrRemoveMaterialSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            currentQuantity = material.quantity_in_stock
+            newQuantity = currentQuantity + serializer.validated_data['quantity']
+            serializer.update(material, {"quantity_in_stock" : newQuantity})
+            return Response({"Message": f"Quantity Successfully updated for material with id {material_id}"})
+        except Material.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Reduce product quantity
+@api_view(['PATCH'])
+def reduceMaterialQuantity(request):
+    serializer = addOrRemoveMaterialSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            currentQuantity = material.quantity_in_stock
+            newQuantity = currentQuantity - serializer.validated_data['quantity']
+            serializer.update(material, {"quantity_in_stock" : newQuantity})
+            return Response({"Message": f"Quantity Successfully updated for material with id {material_id}"})
+        except Material.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Delete product
+@api_view(['DELETE'])
+def deleteMaterial(request):
+    serializer = DeleteProductSerializer(data=request.data)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            material.delete()
+            return Response({"Message": f"Material with id {material_id} successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
+        except Product.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+
 ###############-----------SUPPLIER--------------############
+#Create a new supplier
+@api_view(['POST'])
+def createSupplier(request):
+    serializer = SupplierSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    else:
+        return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+    return Response({"Message": "Supplier successfully created"})
+
+#Retrieve all the suppliers
+@api_view(['GET'])
+def getAllSuppliers(request):
+    suppliers = Supplier.objects.all()
+    serializer = SupplierSerializer(suppliers, many=True)
+    return Response(serializer.data)
+
+#Retrieve a single supplier
+@api_view(['GET'])
+def getSingleSupplier(request):
+    serializer = GetSingleSupplierSerializer(data=request.data)
+    if serializer.is_valid():
+        material_id = serializer.validated_data['id']
+        try:
+            material = Material.objects.get(id=material_id)
+            serializer_data = MaterialSerializer(material, many=False)
+            return Response(serializer_data.data)
+        except Material.DoesNotExist:
+            return Response({"Error": "Material with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Update Supplier
+@api_view(['PATCH'])
+def updateSupplier(request):
+    serializer = UpdateSupplierSerializer(data=request.data, partial=True)
+    if serializer.is_valid():
+        supplier_id = serializer.validated_data['id']
+        try:
+            supplier = Supplier.objects.get(id=supplier_id)
+            updatedSupplier = serializer.update(supplier, serializer.validated_data)
+            return Response({"Message": f"Product with id {supplier_id} successfully updated"})
+        except Supplier.DoesNotExist:
+            return Response({"Error": "Supplier with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
+
+#Delete supplier
+@api_view(['DELETE'])
+def deleteSupplier(request):
+    serializer = DeleteSupplierSerializer(data=request.data)
+    if serializer.is_valid():
+        supplier_id = serializer.validated_data['id']
+        try:
+            supplier = Supplier.objects.get(id=supplier_id)
+            supplier.delete()
+            return Response({"Message": f"Supplier with id {supplier_id} successfully deleted"}, status=status.HTTP_204_NO_CONTENT)
+        except Supplier.DoesNotExist:
+            return Response({"Error": "Supplier with this id not found"}, status=status.HTTP_404_NOT_FOUND)
+    return Response({"Error": "Invalid Payload"}, status=status.HTTP_400_BAD_REQUEST)
